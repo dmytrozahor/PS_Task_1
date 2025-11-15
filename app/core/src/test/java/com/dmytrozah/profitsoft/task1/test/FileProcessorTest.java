fc48c8808ee4c8b6db558d54d83d0d3de6776fdc
Dmytro Za. <dima-zagorodniy@ukr.net>
2025-11-15 10:05:39 +0200

    improve testing

--- app/core/src/test/java/com/dmytrozah/profitsoft/task1/test/FileProcessorTest.java
+++ app/core/src/test/java/com/dmytrozah/profitsoft/task1/test/FileProcessorTest.java
@@ -4,39 +4,88 @@ import com.dmytrozah.profitsoft.task1.core.StatisticsService;
 import com.dmytrozah.profitsoft.task1.core.entities.Bookshelf;
 import com.dmytrozah.profitsoft.task1.core.entities.statistics.StatisticsAttributeType;
 import com.dmytrozah.profitsoft.task1.mapping.EntityFileProcessor;
+import com.dmytrozah.profitsoft.task1.mapping.reader.EntityFSProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
 
+@ExtendWith(MockitoExtension.class)
 public class FileProcessorTest {
 
-    private final StatisticsService statisticsService = new StatisticsService(StatisticsAttributeType.TITLE);
-    private final EntityFileProcessor fileProcessor = new EntityFileProcessor();
+    static final Path TEST_DIR = Path.of("./data/test");
+    static final Path TEST_JSON_PATH = TEST_DIR.resolve("/input/test.json");
+
+    static final String TEST_JSON =
+            """
+            [
+                  {
+                      "title": "Wuthering Heights",
+                      "author": "Emily Brontë",
+                      "year_published": 1847,
+                      "genre": "Gothic, Dark Romance"
+                  },
+                  {
+                      "title": "Wuthering Heights",
+                      "author": "Emily Brontë",
+                      "year_published": 1847,
+                      "genre": "Gothic, Dark Romance"
+                  }
+            ]
+            """;
+
+    @Mock
+    private EntityFSProvider fsProvider;
+
+    private EntityFileProcessor fileProcessor;
+
+    @BeforeEach
+    public void setup() {
+        fileProcessor = new EntityFileProcessor(fsProvider);
+    }
 
     @Test
-    public void correctAggregatedStatistics() {
-        EntityFileProcessor entityFileProcessor = new EntityFileProcessor();
+    public void processingIsCorrect() throws IOException {
+        final Path file = Paths.get(TEST_JSON_PATH.toString());
 
-        entityFileProcessor.init(statisticsService, "./data/test/", 1);
+        Mockito.when(fsProvider.listFiles(eq(TEST_DIR.resolve("input"))))
+                .thenReturn(Stream.of(file));
 
-        entityFileProcessor.processInputFiles(__ -> {});
-        entityFileProcessor.processOutputFiles();
+        Mockito.when(fsProvider.getInputStream(eq(TEST_JSON_PATH)))
+                .thenReturn(new ByteArrayInputStream(TEST_JSON.getBytes()));
 
-        assertEquals(1, statisticsService.getAggregatedStatistics().getOccurrences("Wuthering Heights"));
+        fileProcessor.init(StatisticsAttributeType.TITLE, TEST_DIR.toString(), 1);
+
+        fileProcessor.processInputFiles(__ -> {});
+        fileProcessor.processOutputFiles();
+
+        final StatisticsService statisticsService = fileProcessor.getStatisticsService();
+
+        assertNotNull(statisticsService.getAggregatedStatistics());
+        assertEquals(2, statisticsService.getAggregatedStatistics().getOccurrences("Wuthering Heights"));
     }
 
 
     @Test
-    public void bookExtraction(){
+    public void bookExtraction() throws IOException {
+        Mockito.when(fsProvider.getInputStream(eq(TEST_JSON_PATH)))
+                .thenReturn(new ByteArrayInputStream(TEST_JSON.getBytes()));
+
         final Bookshelf bookshelf = new Bookshelf();
-        bookshelf.setPath("./data/test/input/test.json");
+        bookshelf.setPath(TEST_JSON_PATH.toString());
 
         assertEquals(2, fileProcessor.extractBooks(bookshelf, 50).size());
     }
@@ -46,7 +95,7 @@ public class FileProcessorTest {
         try (final Stream<Path> files = Files.list(Paths.get("./data"))){
             final int fileCount = (int) files.count(); // Very unprobably the file count will exceed Integer.MAX_INT-1
 
-            fileProcessor.init(statisticsService, "./data", fileCount);
+            fileProcessor.init(StatisticsAttributeType.TITLE, "./data", fileCount);
             assertEquals(Math.min(Runtime.getRuntime().availableProcessors(), fileCount), fileProcessor.getThreads());
         }
     }
--- app/core/src/test/java/com/dmytrozah/profitsoft/task1/test/StatisticsServiceTest.java
+++ app/core/src/test/java/com/dmytrozah/profitsoft/task1/test/StatisticsServiceTest.java
@@ -2,28 +2,96 @@ package com.dmytrozah.profitsoft.task1.test;
 
 import com.dmytrozah.profitsoft.task1.core.StatisticsService;
 import com.dmytrozah.profitsoft.task1.core.entities.Bookshelf;
+import com.dmytrozah.profitsoft.task1.core.entities.statistics.Statistics;
 import com.dmytrozah.profitsoft.task1.core.entities.statistics.StatisticsAttributeType;
+import com.dmytrozah.profitsoft.task1.mapping.reader.EntityFSProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 
+@ExtendWith(MockitoExtension.class)
 public class StatisticsServiceTest {
 
-    static final Path TEST_PATH = Paths.get("./data/test/input/test.json");
+    static final Path TEST_PATH = Path.of("./data/test/input/test.json");
 
-    private final StatisticsService statisticsService = new StatisticsService(StatisticsAttributeType.TITLE);
+    @Mock
+    private EntityFSProvider fsProvider;
+
+    private StatisticsService statisticsService;
+
+    static final String TEST_SINGLE_JSON =
+            """
+            [
+                  {
+                      "title": "Wuthering Heights",
+                      "author": "Emily Brontë",
+                      "year_published": 1847,
+                      "genre": "Gothic, Dark Romance"
+                  }
+            ]
+            """;
+
+    static final String TEST_MULTIPLE_JSON =
+            """
+            [
+                  {
+                      "title": "Wuthering Heights",
+                      "author": "Emily Brontë",
+                      "year_published": 1847,
+                      "genre": "Gothic, Dark Romance"
+                  },
+                  {
+                      "title": "Wuthering Heights",
+                      "author": "Emily Brontë",
+                      "year_published": 1847,
+                      "genre": "Gothic, Dark Romance"
+                  }
+            ]
+            """;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        statisticsService = new StatisticsService(fsProvider, StatisticsAttributeType.TITLE);
+    }
 
     @Test
-    public void validAnalyze(){
+    public void validAnalyzeSingle() throws IOException {
+        Mockito.when(fsProvider.getInputStream(eq(TEST_PATH)))
+                .thenReturn(new ByteArrayInputStream(TEST_SINGLE_JSON.getBytes()));
+
         final Bookshelf bookshelf = new Bookshelf();
         bookshelf.setPath(TEST_PATH.toString());
 
-        statisticsService.analyze(bookshelf, TEST_PATH);
+        statisticsService.analyze(TEST_PATH);
 
-        assertEquals(1, statisticsService.getStatistics(bookshelf).getOccurrences("Wuthering Heights"));
+        final Statistics statistics = statisticsService.getStatistics(bookshelf);
+
+        assertEquals(1, statistics.getOccurrences("Wuthering Heights"));
+    }
+
+    @Test
+    public void validAnalyzeMultiple() throws IOException {
+        Mockito.when(fsProvider.getInputStream(eq(TEST_PATH)))
+                .thenReturn(new ByteArrayInputStream(TEST_MULTIPLE_JSON.getBytes()));
+
+        final Bookshelf bookshelf = new Bookshelf();
+        bookshelf.setPath(TEST_PATH.toString());
+
+        statisticsService.analyze(TEST_PATH);
+
+        final Statistics statistics = statisticsService.getStatistics(bookshelf);
+
+        assertEquals(2, statistics.getOccurrences("Wuthering Heights"));
     }
 
 }
